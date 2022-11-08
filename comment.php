139e7843e852c0ea08fd764bbf12bf3f3103efe8
Raph <Raph@gmail.com>
2022-11-08 15:23:52 +0800

    added sql query, display author + message

--- comment.php
+++ comment.php
@@ -1,3 +1,5 @@
+<?php require('dbaccess.php');?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,8 +11,26 @@
 <body>
 
     <div id="comments">
-        
+        <?php 
+            $results = mysqli_query($con, "SELECT * FROM comments LIMIT 2");
+
+            if (mysqli_num_rows($results) > 0) {
+                while ($row = mysqli_fetch_array($results)) {
+        ?>
+            <p> <?php echo $row['author']?> <br> <?php echo $row['message']?> </p>
+
+        <?php
+                
+                }
+            }
+
+            else{
+                echo "there are no comments";
+            }
+        ?>
     </div>
+
+    <button>Show more comments..</button>
 
     
     <!-- JQUERY -->
